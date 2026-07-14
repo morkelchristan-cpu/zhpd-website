c0eb1ec2b7c9cfe39eac666ad463a427a32458d0
{𝔇𝔇}𝕮𝖍𝖗𝖎𝖘 <morkel.christan@gmail.com>
2026-07-15 00:30:19 +0200

    Added new files.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
-import './globals.css';
-import Navbar from '@/src/components/Navbar';
+import SessionWrapper from "@/src/components/SessionWrapper"; // Adjust path if needed
+import Navbar from "@/src/components/Navbar";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <Navbar />
-        {children}
+        <SessionWrapper>
+          <Navbar />
+          {children}
+        </SessionWrapper>
       </body>
     </html>
   );
