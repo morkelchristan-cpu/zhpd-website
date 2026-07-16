71a746e54e48e55922ce85e94bd7de1022e95625
{𝔇𝔇}𝕮𝖍𝖗𝖎𝖘 <morkel.christan@gmail.com>
2026-07-16 20:12:25 +0200

    Fixed anon key.

--- src/components/lib/supabase.ts
+++ src/components/lib/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://dtbuzlggibtuicfuunan.supabase.co/rest/v1/';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0YnV6bGdnaWJ0dWljZnV1bmFuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NDIxNDE0NCwiZXhwIjoyMDk5NzkwMTQ0fQ.Dobfuw00bjNYjMs3M7tQBlodbDOq11x9ps3GxmdW4Ds';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0YnV6bGdnaWJ0dWljZnV1bmFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMTQxNDQsImV4cCI6MjA5OTc5MDE0NH0.1EKqUQ6OopequE4QILGqLHoonjUQi6_ttv1ZRhMpPyo';
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase credentials!");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
